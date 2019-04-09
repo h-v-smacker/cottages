0915fbbbadc04d31b0a8b7b67f7b732b9abec692
Er Stazi <71944+erstazi@users.noreply.github.com>
2019-04-09 09:45:54 -0400

    Updating with the ability for farming_undo to work with cottages

--- init.lua
+++ init.lua
@@ -76,6 +76,13 @@ if farming.mod and farming.mod == "redo" then
 	cottages.handmill_product[ "farming:rice" ] = 'farming:rice_flour 1';
 end
 
+if farming.mod and farming.mod == "undo" then
+	cottages.handmill_product[ "farming:seed_oat" ] = 'farming:flour 1';
+	cottages.handmill_product[ "farming:seed_rye" ] = 'farming:flour 1';
+	cottages.handmill_product[ "farming:seed_rice" ] = 'farming:rice_flour 1';
+	cottages.handmill_product[ "farming:rice" ] = 'farming:rice_flour 1';
+end
+
 --[[ some examples:
 cottages.handmill_product[ 'default:cobble' ] = 'default:gravel';
 cottages.handmill_product[ 'default:gravel' ] = 'default:sand';
